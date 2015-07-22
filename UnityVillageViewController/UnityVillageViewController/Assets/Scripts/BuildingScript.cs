1726f79f65c622db1bfff7a2794702dd9b536d81
iProphet <heeply@yahoo.co.uk>
2015-07-22 10:31:38 +0300

    Adding simple 2d Village view controller

---
+++ UnityVillageViewController/UnityVillageViewController/Assets/Scripts/BuildingScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class BuildingScript : MonoBehaviour {
+
+	public SpriteRenderer parentRenderer;
+
+	private string type;
+	private string id;
+	private string keyString;
+	private Vector2 position;
+	private SpriteRenderer spriteRenderer;
+
+	private JSONNode node;
+
+	// Use this for initialization
+	public void Init (JSONNode nodeData)
+	{
+		node = nodeData;
+
+		spriteRenderer.sprite = Resources.Load<Sprite>("Images/Buildings/" + node["buildingImage"]);
+		keyString = node["keyString"];
+		type = node["type"];
+		float buildingPosX = node["x"].AsFloat;
+		float buildingPosY = node["y"].AsFloat;
+
+		Vector3 buildingTransform = transform.position;
+		buildingTransform.x = buildingPosX;
+		buildingTransform.y = -buildingPosY;
+
+		transform.position = buildingTransform;
+		spriteRenderer.sortingOrder = 2;
+
+		if (type.Equals ("MOA")) {
+			spriteRenderer.sortingOrder = 1;
+		} else if (type.Equals ("GAT")) {
+			spriteRenderer.sortingOrder = 3;
+		} else {
+			spriteRenderer.sortingOrder = 2;
+		}
+	}
+
+	void Awake () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+
+	
+	void Start()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ UnityVillageViewController/UnityVillageViewController/Assets/Scripts/VillageCamera.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillageCamera : MonoBehaviour {
+	public float ScrollSpeed = 10f;
+	public float ScrollEdge = 0.01f;
+
+	public GameObject parent;
+
+	private SpriteRenderer parentRenderer;
+	
+	void Start()
+	{
+		parentRenderer = parent.GetComponent<SpriteRenderer> ();
+	}
+
+	void Update ()
+	{
+		if (Input.mousePosition.x >= Screen.width * (1 - ScrollEdge))
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
+		}
+		else if (Input.mousePosition.x <= Screen.width * ScrollEdge)
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * -ScrollSpeed, Space.World);
+		}
+		
+		if (Input.mousePosition.y >= Screen.height * (1 - ScrollEdge))
+		{
+			transform.Translate(Vector3.up * Time.deltaTime * ScrollSpeed, Space.World);
+		}
+		else if (Input.mousePosition.y <= Screen.height * ScrollEdge)
+		{ 
+			transform.Translate(Vector3.up * Time.deltaTime * -ScrollSpeed, Space.World);
+		}
+
+		Vector3 clampPosition = transform.position;
+		Vector2 cameraMin = Camera.main.BoundsMin();
+		Vector2 cameraMax = Camera.main.BoundsMax();
+
+		if (cameraMin.x < parentRenderer.bounds.min.x) {
+			clampPosition.x = parentRenderer.bounds.min.x + Camera.main.Extents().x;
+		} else if (cameraMax.x > parentRenderer.bounds.max.x) {
+			clampPosition.x = parentRenderer.bounds.max.x - Camera.main.Extents().x;
+		}
+
+		if (cameraMin.y < parentRenderer.bounds.min.y) {
+			clampPosition.y = parentRenderer.bounds.min.y  + Camera.main.Extents().y;
+		} else if (cameraMax.y > parentRenderer.bounds.max.y) {
+			clampPosition.y = parentRenderer.bounds.max.y - Camera.main.Extents().y;
+		}
+
+		transform.position = clampPosition;
+	}
+}
